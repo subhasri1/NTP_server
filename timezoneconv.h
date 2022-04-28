4474f154b6747918cae7422d46389a28a96c1c3e
Kanwarsidhu10 <snehkanwar@live.com>
2022-04-28 11:42:45 +0530

    SNEHKANWAR=>Added epoch to cliend based timezome convertor functionality(NTP_14)

---
+++ timezoneconv.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<time.h>
+#define fsize 256
+
+using namespace std;
+
+void timezoneconv(long &tot , char buffer[fsize])
+{
+	time_t xepoch_time=tot;
+	
+	/*CONVERT To Struct tm */
+	struct tm *converted_time;
+	converted_time = localtime(&xepoch_time);
+	
+	tzset();//setting time
+	
+	strftime(buffer,26,"%Y-%m-%d %H:%M:%S",converted_time);
+	cout<<"TIME STAMP ACCORDING TO SYSTEM TIMEZONE=>"<<tzname[converted_time->tm_isdst]<<endl;
+	//timezonetime=buffer+" "+tzname[converted_time->tm_isdst];
+
+	return ;
+}
