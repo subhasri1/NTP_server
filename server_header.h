4a8bf31b38f5f6fe35923ca736a4193b571f1da9
Kanwarsidhu10 <snehkanwar@live.com>
2022-05-02 00:37:50 +0530

    SNEHKANWAR=>Completed all the modules

---
+++ server_header.h
@@ -0,0 +1,32 @@
+#include<iostream>//system call, data types
+using namespace std;
+#include<sys/socket.h>//socket
+#include<unistd.h>//read & write
+#include<arpa/inet.h>//ip
+#include<fstream>//filehandling
+#include<map>//map STL
+#include<cstring>//strcpy
+#include<stdlib.h>//atoi
+
+#include<ctime>
+
+void user_cred_file_to_map(map<string, string> &user_cred);//key,value--->userid,password
+
+void login_and_registration_of_user(int cli_des,map<string, string> userCred);
+
+char user_cred_authentication(map<string, string> user_cred, char data[],int n);
+
+void user_cred_addition(map<string, string> &user_cred,char new_user_info[],int k);
+
+time_t gettimefromLocalMachine();
+
+time_t gettimefromserver(char* name );
+
+
+
+
+void best_time_to_GMT_conversion();
+
+time_t GmT_to_epoch_coversion(tm *best_time_gmt);
+
+
